e9d05f3323ca205fb862394b1c2d0c77b886c6b5
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2024-05-03 19:51:23 +0200

    Functionality

--- src/App.vue
+++ src/App.vue
@@ -40,31 +40,59 @@ const questions = ref([
 const quizCompleted = ref(false)
 const currentQuestion = ref(0)
 const score = computed(() => {
-  let value = 0
-  questions.value.map(q => {
-    if (q.selected == q.answer) {
-      value++
-    }
-  })
-  return value
+    let value = 0
+    questions.value.map(q => {
+        if (q.selected == q.answer) {
+            value++
+        }
+    })
+    return value
 })
+const getCurrentQuestion = computed(() => {
+    let question = questions.value[currentQuestion.value]
+    question.index = currentQuestion.value
+    return question
+})
+
+const setAnswer = evt => {
+    questions.value[currentQuestion.value].selected = evt.target.value
+    evt.target.value = null
+}
+
+const NextQuestion = () => {
+    if (currentQuestion.value < question.value.length - 1) {
+        currentQuestion.value++
+    } else {
+        quizCompleted.value = true
+    }
+}
 </script>
 
 <template>
-  <h1>Hello world</h1>
-  <p>Witoj</p>
+    <main class="app">
+        <h1>The quiz</h1>
+
+        <section class="quiz"></section>
+            <div class="quiz-info">
+                <span class="question">{{ getCurrentQuestion.question }}</span>
+                <span class="score"> Score {{ score }} / {{ questions.length }}</span>
+            </div>
+            
+
+    </main>
+
 </template>
 
 <style>
 * { 
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Montserrat', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
 }
 
 body {
-  background-color: #271c36;
-  color: #FFF;
+    background-color: #271c36;
+    color: #FFF;
 }
 </style>
