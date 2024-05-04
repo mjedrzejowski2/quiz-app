a6ebcacf9534c1adaffe70c285acf871ca6ffa01
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2024-05-04 17:55:05 +0200

    Implementing the functionality into website

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ const setAnswer = evt => {
 }
 
 const NextQuestion = () => {
-    if (currentQuestion.value < question.value.length - 1) {
+    if (currentQuestion.value < questions.value.length - 1) {
         currentQuestion.value++
     } else {
         quizCompleted.value = true
@@ -72,19 +72,64 @@ const NextQuestion = () => {
     <main class="app">
         <h1>The quiz</h1>
 
-        <section class="quiz"></section>
+        <section class="quiz" v-if="!quizCompleted">
             <div class="quiz-info">
                 <span class="question">{{ getCurrentQuestion.question }}</span>
                 <span class="score"> Score {{ score }} / {{ questions.length }}</span>
             </div>
-            
+            <div class="options">
+                <label
+                    v-for="(option, index) in getCurrentQuestion.options"
+                    :key="index"
+                    :class="`option ${
+                        getCurrentQuestion.selected == index
+                            ? index == getCurrentQuestion.answer
+                                ? 'correct'
+                                : 'wrong'
+                            : ''
+                    } ${
+                        getCurrentQuestion.selected != null &&
+                        index != getCurrentQuestion.selected
+                            ? 'disabled'
+                            : ''
 
+                    }`">
+
+                    <input
+                        type="radio"
+                        :name="getCurrentQuestion.index"
+                        :value="index"
+                        v-model="getCurrentQuestion.selected"
+                        :disabled="getCurrentQuestion.selected"
+                        @change="setAnswer">
+                    <span>{{ option }}</span>
+                </label>
+            </div>
+
+            <button
+                @click="NextQuestion"
+                :disabled="!getCurrentQuestion.selected">
+                {{
+                    getCurrentQuestion.index == questions.length - 1
+                    ? 'Finish'
+                    : getCurrentQuestion.selected == null
+                        ? 'Select an option'
+                        : 'Next question'
+                }}
+            </button>
+        </section>
+        <section v-else>
+            <h2>You have finished the quiz!</h2>
+            <p>Your score is {{ score }} / {{ questions.length }}</p>
+
+        </section>
+        
     </main>
 
 </template>
 
 <style>
-* { 
+*{ 
     margin: 0;
     padding: 0;
     box-sizing: border-box;
